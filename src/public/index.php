ca0adb786fdcf190bd1bed5ffedbaeeb59898d91
Tudor Voicu Bradu <tvbradu@gmail.com>
2021-07-21 11:19:45 +0300

    post json, respond json

--- src/public/index.php
+++ src/public/index.php
@@ -1,14 +1,25 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
 
 require __DIR__.'/../../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+//request handler
+$app->addBodyParsingMiddleware();
 $app->get('/', function (Request $request, Response $response, $args) {
   $response->getBody()->write("Hello world!");
+  return $response;
+});
+
+$app->post('/', function (Request $request, Response $response, $args){
+  $body = $request->getParsedBody();
+  //var_dump($body);die();
+  $response->getBody()->write(json_encode($body));
+  $response->withHeader('Content-type', 'application/json');
   return $response;
 });
 
